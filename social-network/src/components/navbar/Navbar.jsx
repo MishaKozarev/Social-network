9ebcf46e1afa68a966c2c3caf3d49819d2670c26
MishaKozarev <mishaaaaa1986@mail.ru>
2023-09-26 10:41:04 +0300

    feat: add component header, profile, navbar

---
+++ social-network/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <aside className="navbar">
+      <a className="navbar__link" href="#navbar">Profile</a>
+      <a className="navbar__link" href="#navbar">Profile</a>
+      <a className="navbar__link" href="#navbar">Profile</a>
+    </aside>
+  );
+}
+
+export default Navbar;
---
+++ social-network/src/components/profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './profile.css'
+
+const Profile = () => {
+  return (
+    <aside className="profile">
+    </aside>
+  );
+}
+
+export default Profile;
